8194c9200f01071b633422476942d19b4918d5b5
Fellipe Calleia <fellipe@calleia.com.br>
2022-10-23 14:55:10 -0300

    Add hash to json

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-window.onclick = function(e) {
+async function digestMessage(message) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+window.onclick = async function(e) {
     var object = new Object();
+    object.hash = await digestMessage(text.value);
     object.value = text.value;
 
     var jsonString = JSON.stringify(object);
